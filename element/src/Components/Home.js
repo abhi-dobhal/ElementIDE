17081ea4d06ab4a589c292832ed95c9e757e43e1
Abhishekdobhal <abhi.dobhal2@gmail.com>
2023-10-05 16:05:09 +0530

    A IDE Clone

---
+++ element/src/Components/Home.js
@@ -0,0 +1,16 @@
+import Codex from "./Codex";
+import Header from "./Header";
+import Result from "./Result";
+
+const Home =()=>{
+    return (
+        <>
+         <Header/>
+        <Codex/>
+        <Result/>
+        </>
+       
+    )
+}
+
+export default Home;
---
+++ element/src/Components/Result.js
@@ -0,0 +1,9 @@
+
+
+const Result=()=>{
+    return(
+        <div>hello</div>
+    )
+}
+
+export default Result;
